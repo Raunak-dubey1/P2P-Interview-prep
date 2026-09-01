fc8a514f2c44996aedaaf0ff1c67c11f98d55d3e
Raunak Dubey <soulraunak8051@gmail.com>
2026-09-02 00:39:39 +0530

    Enhance session management: add active session retrieval and recent session query

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -3,6 +3,7 @@ import Session from "../models/Session.js";
 
 
 export async function createSession(req,res){
+
     try{
         const {problem,difficulty}=req.body;
         if(!problem) res.status(400).json({message:"Problem is required to create Session"});
@@ -45,21 +46,59 @@ export async function createSession(req,res){
 }
 
 export async function joinSession(req,res){
+    try{
 
+    }catch(error){
+        
+    }
 }
 
-export async function getActiveSessions(req,res){
+export async function getActiveSessions(_,res){ //since we do not need req we put _
+    try{
+        const session=await Session.find({status:"active"})
+        .populate("host","name profileImage") //populating means adding more details 
+        .sort({createdAt:-1})
+        .limit(5);                                      //here we are the details of host  
+        
 
+        res.status(200).json({session});
+    }catch(error){                            
+        console.error("Error while handling getActiveSession Controller",error);
+        res.status(500).json({msg:"Internal Server Error"});
+    }
 }
 
 export async function endSession(req,res){
+    try{
 
+    }catch(error){
+
+    }
 }
 
 export async function getSessionById(req,res){
+    try{
+
+    }catch(error){
+        
+    }
 
 }
 
 export async function getMyRecentSession(req,res){
+    try{
+        //Note: here we have to check both as host and the participant
+        const userID=req.user._id;        
+        const session=Session.find({
+            status:"completed",
+            $or:[{host:"userID"},{participant:"userID"}],
+        }).sort({createdAt:-1})
+          .limit(10);
+
+          res.status(200).json({session});
+    }catch(error){
+        console.log("Error while getting recents session that has been done");
+        res.status(500).json({msg:"Internal Server Error"});
+    }
 
 }
