f489e83c75fd8deb2b926d5ef24c2497a7059b89
Raunak Dubey <soulraunak8051@gmail.com>
2026-09-13 22:42:35 +0530

    Just Change in path for logged in User

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './Pages/HomePage.jsx'
 import ProblemsPage from './Pages/ProblemsPage.jsx';
 import ProblemPage from './Pages/ProblemPage.jsx';
 import DashboardPage from './Pages/DashboardPage.jsx';
+import SessionPage from './Pages/SessionPage.jsx'
 
 import { Toaster } from 'react-hot-toast';
 import { useUser } from '@clerk/clerk-react';
@@ -15,10 +16,11 @@ function App() {
   return (
     <>
     <Routes>
-     <Route path="/" element={<HomePage/>}/> 
-     <Route path="/problems" element={isSignedIn?<ProblemsPage/>:<Navigate to="/"/>}/>
+      <Route path="/" element={!isSignedIn?<HomePage/>:<Navigate to="/dashboard"/>}/> 
+      <Route path="/problems" element={isSignedIn?<ProblemsPage/>:<Navigate to="/"/>}/>
       <Route path="/problem/:id" element={isSignedIn?<ProblemPage/>:<Navigate to="/"/>}/>
-       <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+      <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+      <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
     </Routes>
     <Toaster/>
     </>
