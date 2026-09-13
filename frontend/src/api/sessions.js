a69f59c6370455e2ed30c8ff88d58fde611e6243
Raunak Dubey <soulraunak8051@gmail.com>
2026-09-13 22:48:16 +0530

    Added try catch block to identify error while hadling api

--- frontend/src/api/sessions.js
+++ frontend/src/api/sessions.js
@@ -1,35 +1,73 @@
-import axiosInstance from "../lib/axios";
+import axiosInstance from "../lib/axios.js";
 
 export const sessionApi = {
   createSession: async (data) => {
-    const response = await axiosInstance.post("/sessions", data);
-    return response.data;
+    try {
+      const response = await axiosInstance.post("/sessions", data);
+      return response.data;
+    } catch (error) {
+      console.log("Error in createSession api handling", error);
+      throw error;
+    }
   },
 
   getActiveSessions: async () => {
-    const response = await axiosInstance.get("/sessions/active");
-    return response.data;
+    try {
+      const response = await axiosInstance.get("/sessions/active");
+      return response.data;
+    } catch (error) {
+      console.log("Error while getActive session api handling", error);
+      throw error;
+    }
   },
+
   getMyRecentSessions: async () => {
-    const response = await axiosInstance.get("/sessions/my-recent");
-    return response.data;
+    try {
+      const response = await axiosInstance.get("/sessions/my-recent");
+      return response.data;
+    } catch (error) {
+      console.log("Error while getRecent session api handling", error);
+      throw error;
+    }
   },
 
   getSessionById: async (id) => {
-    const response = await axiosInstance.get(`/sessions/${id}`);
-    return response.data;
+    try {
+      const response = await axiosInstance.get(`/sessions/${id}`);
+      return response.data;
+    } catch (error) {
+      console.log("Error while getSessionBYid api handling", error);
+      throw error;
+    }
   },
 
   joinSession: async (id) => {
-    const response = await axiosInstance.post(`/sessions/${id}/join`);
-    return response.data;
+    try {
+      const response = await axiosInstance.post(`/sessions/${id}/join`);
+      return response.data;
+    } catch (error) {
+      console.log("Error while join session api handling", error);
+      throw error;
+    }
   },
+
   endSession: async (id) => {
-    const response = await axiosInstance.post(`/sessions/${id}/end`);
-    return response.data;
+    try {
+      const response = await axiosInstance.post(`/sessions/${id}/end`);
+      return response.data;
+    } catch (error) {
+      console.log("Error while end session api handling", error);
+      throw error;
+    }
   },
+
   getStreamToken: async () => {
-    const response = await axiosInstance.get(`/chat/token`);
-    return response.data;
+    try {
+      const response = await axiosInstance.get(`/chat/token`);
+      return response.data;
+    } catch (error) {
+      console.log("Error while getStreamToken api handling", error);
+      throw error;
+    }
   },
 };
